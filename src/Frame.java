98214bc21b60e8cc12d9c12057971d2e556e9bbf
mohammad_amiri <mohammad2005amiri@gmail.com>
2024-03-15 23:57:08 +0330

    add func give score and some func about thatc

--- src/Frame.java
+++ src/Frame.java
@@ -4,11 +4,40 @@ public class Frame {
     private int column ;//weight of a block
     private char[][] frame ;
 
+    private int score ;
+
     public Menu menu = new Menu();
-    public Frame( int row , int column) {
+
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Frame(int row , int column) {
         this.row = row;
         this.column = column;
         this.frame = new char[row][column] ;
+        this.score = 0 ;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
 
@@ -152,10 +181,90 @@ public class Frame {
         addBlock(block);
     }
 
+    public void giveScore(Frame frame){
+
+        while (true ){
+            int col = checkColumn(frame);
+            if (col == -1){
+                break;
+            }
+            else
+            {
+                clearColumn(col,frame);
+                this.score = this.score - 10;
+            }
+        }
+
+        while (true ){
+            int row = checkRow(frame);
+            if (row == -1){
+                break;
+            }
+            else
+            {
+                clearRow(row , frame);
+                this.score = this.score + 100 ;
+            }
+        }
 
 
+    }
 
+    public int checkRow(Frame frame){
+        boolean flag;
+        for (int i = 3; i < this.row - 1; i++) {
+            flag = true;
+            for (int j = 0; j < this.column; j++) {
+                if (this.frame[i][j] != '*' ){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                return i;
+            }
+        }
+        return -1;
 
+    }
 
+    public int checkColumn(Frame frame){
+        boolean flag;
+        for (int i = 1; i < this.column - 1; i++) {
+            flag = true;
+            for (int j = 2; j < this.row - 1; j++) {
+                if (this.frame[j][i] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void checkFirstColumn(Frame frame) {
+
+        for (int i = 1; i < this.column - 1; i++) {
+            if (this.frame[0][i] == '*'){
+                clearColumn(i , frame);
+            }
+            this.score = this.score - 10;
+        }
+
+    }
+    public void clearColumn(int col , Frame frame){
+        for (int i = 0; i < this.row - 1; i++) {
+            this.frame[i][col] = ' ';
+        }
+    }
+
+    public void clearRow( int ro , Frame frame){
+        for (int i = 0; i <this.column ; i++) {
+            this.frame[ro][i] = ' ';
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        Frame frame = new Frame(19,10);
+        Frame frame = new Frame(18,10);
 
         while (true){
             Block block = new Block();
             frame.addBlock(block);
+            for (int i = 0; i < 3; i++) {
+
+                if (frame.isValidMoveDown(block)){
+                    block.setRow(block.getRow() - 1);
+                    frame.moveDown(block);
+                }
+                else
+                {
+                    frame.checkFirstColumn(frame);
+                }
+            }
             frame.printFrame();
+            System.out.println("_______________________________");
             while (frame.isValidMoveDown(block)){
                 block.setRow(block.getRow() - 1);
+                frame.giveScore(frame);
                 frame.menu.choseMove(frame,block);
                 frame.printFrame();
+                System.out.println( "Your score is: " + frame.getScore());
                 System.out.println("_______________________________");
             }
         }
